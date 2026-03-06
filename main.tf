3323e03253ea0879530c39d90e47913751433a8d
sksinghsujit <sksingh.sujit@gmail.com>
2026-03-06 15:03:13 +0400

    update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "kubernetes" {
   token = var.K8S_TOKEN
   # If using self-signed certs, you might need:
   # insecure = true 
-  cluster_ca_certificate = K8S_CA_CERT
+  cluster_ca_certificate = var.K8S_CA_CERT
 }
 
 resource "kubernetes_namespace" "app_ns" {
