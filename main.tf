37906de06b791ae53f3aacc90e9043390d58f33f
sksinghsujit <sksingh.sujit@gmail.com>
2026-03-09 13:00:27 +0400

    updates

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+variable "K8S_HOST" {}
+variable "K8S_TOKEN" {}
+variable "K8S_CA_CERT" {}
+
 module "dbs_cluster" {
   source = "./modules/dbs-cluster"
   # These variables are automatically pulled from your HCP Workspace
--- variables.tf
+++
@@ -1,3 +0,0 @@
-variable K8S_HOST {}
-variable K8S_TOKEN {}
-variable K8S_CA_CERT {}
