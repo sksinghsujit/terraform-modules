2b4505928035a5aaa0359c85f7b2f815e6e8a617
sksinghsujit <sksingh.sujit@gmail.com>
2026-03-09 13:05:13 +0400

    move kubernetes provider to root main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ variable "K8S_HOST" {}
 variable "K8S_TOKEN" {}
 variable "K8S_CA_CERT" {}
 
+
+provider "kubernetes" {
+  host  = var.K8S_HOST
+  token = var.K8S_TOKEN
+  # If using self-signed certs, you might need:
+  # insecure = true 
+  cluster_ca_certificate = var.K8S_CA_CERT
+}
+
 module "dbs_cluster" {
   source = "./modules/dbs-cluster"
   # These variables are automatically pulled from your HCP Workspace
